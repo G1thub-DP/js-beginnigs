d096d78d926d1415ed237d578b22ebb4f61a792d
D. Devi Prasad <118455529+G1thub-DP@users.noreply.github.com>
2023-09-24 07:06:08 +0000

    container config

--- start/test.js
+++ start/test.js
@@ -1,1 +1,16 @@
-console.log("Devi")
+const accountId = 100
+let accountEmail = "devi@gmail.com"
+var accountPassword = "1234"
+accountCity = "Kharagpur"
+let accountState
+
+//accountId = 2 changing the const is not allowed
+
+accountEmail = "abc@gmail.com"
+accountPassword = "4321"
+accountCity = "Bbsr"
+
+console.log("accountId")
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
